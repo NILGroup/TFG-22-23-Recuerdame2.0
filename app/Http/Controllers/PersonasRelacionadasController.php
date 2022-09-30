0ca94d4eb415b0e9e4a969949ec10276fd7d3e6c
erosguerrero <erosjgs@gmail.com>
2022-09-30 14:19:40 +0200

    Basic Controllers
    Some Controllers with some functions

---
+++ app/Http/Controllers/PersonasRelacionadasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Personarelacionada;
+use Illuminate\Http\Request;
+
+class PersonasRelacionadasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Personarelacionada  $personarelacionada
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Personarelacionada $personarelacionada)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Personarelacionada  $personarelacionada
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Personarelacionada $personarelacionada)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Personarelacionada  $personarelacionada
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Personarelacionada $personarelacionada)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Personarelacionada  $personarelacionada
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Personarelacionada $personarelacionada)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/RecuerdosController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Recuerdo;
+use Illuminate\Http\Request;
+
+class RecuerdosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Recuerdo  $recuerdo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Recuerdo $recuerdo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Recuerdo  $recuerdo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Recuerdo $recuerdo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Recuerdo  $recuerdo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Recuerdo $recuerdo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Recuerdo  $recuerdo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Recuerdo $recuerdo)
+    {
+        //
+    }
+}
