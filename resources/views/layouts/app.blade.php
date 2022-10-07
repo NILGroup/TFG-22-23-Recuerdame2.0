968039f41517d44190a6dbeffac5b5ba36383f63
Adrián Prieto <72133257+Lurngi@users.noreply.github.com>
2022-10-07 11:42:33 +0200

    Index pacientes a plantilla blade con header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,23 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
+    <title>Recuérdame</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <script src="public/bootstrap-5.1.3-dist/js/bootstrap.js"></script>
+    <!-- Styles 
+    <style>
+        
+
+    </style>-->
+
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
--- resources/views/pacientes/index.blade.php
+++ resources/views/pacientes/index.blade.php
@@ -1,29 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Recuérdame</title>
-
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-         <script src="public/bootstrap-5.1.3-dist/js/bootstrap.js"></script>
-        <!-- Styles 
-        <style>
-           
-
-        </style>-->
-
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
-    </head>
-    <body class="d-flex flex-column min-vh-100">
+@section('content')
         <div class="contenedor">
             <div class="pt-4 pb-2">
                 <h5 class="text-muted">Listado de pacientes</h5>
@@ -51,7 +28,7 @@
                         <th scope="col"></th>
                     </tr>
                 </thead>
-                <tbody>
+                <!--<tbody>-->
                 @foreach($pacientes as $paciente)
                     <tr>
                     
@@ -78,12 +55,15 @@
                                 <!--<a href="gestor.php?accion=eliminarPaciente&idPaciente=<!?php echo ($row['id_paciente']) ?>">--><i class="fa-solid fa-trash-can text-danger tableIcon"></i></a>
                         </td>
                         <?php   $i++; ?>
-                        @endforeach
                     </tr>
-                   
-                          
+                @endforeach
+                
+
+            </table>
+        </div>
+        
+
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    </body>
-</html>
+@endsection
