fc15a64911716eacd74da6ab82aa60158322a475
SerArias <serarias@ucm.es>
2022-10-09 18:08:50 +0200

    Reajuste avatar

--- resources/views/pacientes/show.blade.php
+++ resources/views/pacientes/show.blade.php
@@ -27,76 +27,77 @@
         <form method="get" action="/pacientes/{{$paciente->id}}">
             <div class="card p-4 h-80">
                 <div class="row justify-content-center p-3">
-                    <img src="/img/avatar_hombre.png" alt="Avatar" class="avatar img-thumbnail">
+                    <div class="row col-sm-6 col-md-4 col-lg-2">
+                        <img src="<?php if ($paciente->genero == 'H') echo '/img/avatar_hombre.png';
+                                    else if ($paciente->genero == 'M') echo '/img/avatar_mujer.png'; ?> " alt="Avatar" class="avatar img-thumbnail">
+                    </div>
+                    <div class="row form-group justify-content-between">
+                        <div class="row col-sm-12 col-md-6 col-lg-5">
+                            <label for="nombre" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-6">Nombre</label>
+                            <div class="col-sm-12 col-md-12 col-lg-6">
+                                <input type="text" disabled class="form-control form-control-sm" id="nombre" value="{{$paciente->nombre}}">
+                            </div>
+                        </div>
+
+                        <div class="row col-sm-12 col-md-6 col-lg-7">
+                            <label for="estado" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-4">Apellidos</label>
+                            <div class="col-sm-12 col-md-12 col-lg-8">
+                                <input type="text" disabled class="form-control form-control-sm" id="apellidos" value="{{$paciente->apellidos}}">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row form-group justify-content-between">
+                        <div class="row col-sm-12 col-md-6 col-lg-5">
+                            <label for="nombre" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-6">Género</label>
+                            <div class="col-sm-12 col-md-12 col-lg-6">
+                                <input type="text" disabled class="form-control form-control-sm" id="genero" value="<?php if ($paciente->genero == 'H') echo 'Hombre';
+                                                                                                                    else if ($paciente->genero == 'M') echo 'Mujer'; ?>  ">
+                            </div>
+                        </div>
+
+                        <div class="row col-sm-12 col-md-6 col-lg-7">
+                            <label for="estado" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-4">Lugar de nacimiento</label>
+                            <div class="col-sm-12 col-md-12 col-lg-8">
+                                <input type="text" disabled class="form-control form-control-sm" id="lugarNacimiento" value="{{$paciente->lugar_nacimiento}}">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row form-group justify-content-between">
+                        <div class="row col-sm-12 col-md-6 col-lg-5">
+                            <label for="fechaNacimiento" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-6">Fecha de nacimiento</label>
+                            <div class="col-sm-12 col-md-12 col-lg-6">
+                                <input type="text" disabled class="form-control form-control-sm" id="fechaNacimiento" value="{{$paciente->fecha_nacimiento}}">
+                            </div>
+                        </div>
+
+                        <div class="row col-sm-12 col-md-6 col-lg-7">
+                            <label for="nacionalidad" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-4">Nacionalidad</label>
+                            <div class="col-sm-12 col-md-12 col-lg-8">
+                                <input type="text" disabled class="form-control form-control-sm" id="nacionalidad" value="{{$paciente->nacionalidad}}">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row form-group justify-content-between">
+                        <div class="row col-sm-12 col-md-6 col-lg-5">
+                            <label for="tipoResidencia" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-6">Tipo de residencia</label>
+                            <div class="col-sm-12 col-md-12 col-lg-6">
+                                <input type="text" disabled class="form-control form-control-sm" id="tipoResidencia" value="{{$paciente->tipo_residencia}}">
+                            </div>
+                        </div>
+                        <div class="row col-sm-12 col-md-6 col-lg-7">
+                            <label for="residenciaActual" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-4">Residencia actual</label>
+                            <div class="col-sm-12 col-md-12 col-lg-8">
+                                <input type="text" disabled class="form-control form-control-sm" id="residenciaActual" value="{{$paciente->residencia_actual}}">
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="row form-group justify-content-between">
-                    <div class="row col-sm-12 col-md-6 col-lg-5">
-                        <label for="nombre" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-6">Nombre</label>
-                        <div class="col-sm-12 col-md-12 col-lg-6">
-                            <input type="text" disabled class="form-control form-control-sm" id="nombre" value="{{$paciente->nombre}}">
-                        </div>
-                    </div>
-
-                    <div class="row col-sm-12 col-md-6 col-lg-7">
-                        <label for="estado" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-4">Apellidos</label>
-                        <div class="col-sm-12 col-md-12 col-lg-8">
-                            <input type="text" disabled class="form-control form-control-sm" id="apellidos" value="{{$paciente->apellidos}}">
-                        </div>
-                    </div>
+                <div class="col-12">
+                    <a href="{{route('pacientes.index')}}"><button type="button" class="btn btn-primary btn-sm">Atrás</button></a>
                 </div>
-
-                <div class="row form-group justify-content-between">
-                    <div class="row col-sm-12 col-md-6 col-lg-5">
-                        <label for="nombre" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-6">Género</label>
-                        <div class="col-sm-12 col-md-12 col-lg-6">
-                            <!--TODO: distinguir entre hombre y mujer -->
-                            <input type="text" disabled class="form-control form-control-sm" id="genero" value="<?php if ($paciente->genero == 'H') echo 'Hombre';
-                                                                                                                else if ($paciente->genero == 'M') echo 'Mujer'; ?>  ">
-                        </div>
-                    </div>
-
-                    <div class="row col-sm-12 col-md-6 col-lg-7">
-                        <label for="estado" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-4">Lugar de nacimiento</label>
-                        <div class="col-sm-12 col-md-12 col-lg-8">
-                            <input type="text" disabled class="form-control form-control-sm" id="lugarNacimiento" value="{{$paciente->lugar_nacimiento}}">
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row form-group justify-content-between">
-                    <div class="row col-sm-12 col-md-6 col-lg-5">
-                        <label for="fechaNacimiento" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-6">Fecha de nacimiento</label>
-                        <div class="col-sm-12 col-md-12 col-lg-6">
-                            <input type="text" disabled class="form-control form-control-sm" id="fechaNacimiento" value="{{$paciente->fecha_nacimiento}}">
-                        </div>
-                    </div>
-
-                    <div class="row col-sm-12 col-md-6 col-lg-7">
-                        <label for="nacionalidad" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-4">Nacionalidad</label>
-                        <div class="col-sm-12 col-md-12 col-lg-8">
-                            <input type="text" disabled class="form-control form-control-sm" id="nacionalidad" value="{{$paciente->nacionalidad}}">
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row form-group justify-content-between">
-                    <div class="row col-sm-12 col-md-6 col-lg-5">
-                        <label for="tipoResidencia" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-6">Tipo de residencia</label>
-                        <div class="col-sm-12 col-md-12 col-lg-6">
-                            <input type="text" disabled class="form-control form-control-sm" id="tipoResidencia" value="{{$paciente->tipo_residencia}}">
-                        </div>
-                    </div>
-                    <div class="row col-sm-12 col-md-6 col-lg-7">
-                        <label for="residenciaActual" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-4">Residencia actual</label>
-                        <div class="col-sm-12 col-md-12 col-lg-8">
-                            <input type="text" disabled class="form-control form-control-sm" id="residenciaActual" value="{{$paciente->residencia_actual}}">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12">
-                <a href="{{route('pacientes.index')}}"><button type="button" class="btn btn-primary btn-sm">Atrás</button></a>
-            </div>
         </form>
     </div>
 </body>
