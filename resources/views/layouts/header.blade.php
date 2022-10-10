c9e92bf267bdbe56a92ef22b27c6db22c1005086
Adrián Prieto <72133257+Lurngi@users.noreply.github.com>
2022-10-10 12:01:23 +0200

    Corrección

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -37,6 +37,7 @@
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                                 document.getElementById('logout-form').submit();">
+                            Cerrar sesión
                                 <i class="fa-solid fa-right-from-bracket"></i>
                             </a>
                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
