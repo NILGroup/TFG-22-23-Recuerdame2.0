a7a3673b5cbce33cb39e100995b3d5a32f256320
Adrián Prieto <72133257+Lurngi@users.noreply.github.com>
2022-10-29 15:41:34 +0200

    Footer arreglado y fast scroll up

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,7 @@
-<div style="height:50px;">
-     <footer class="mt-auto text-lg-start text-end opacity-75 bg-secondary">
-        <div class="p-2">
-            <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Licencia Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a>
-            <?php echo date('Y'); ?> Recuérdame2.0
-        </div>
-    </footer>
-</div>
+<footer class="mt-auto text-end">
+    <a href="#"><i class="fa-solid fa-circle-arrow-up mx-3 mb-2 scrollUp"></i></button>
+    <div class="p-2 bg-secondary text-lg-start">
+        <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Licencia Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a>
+        <?php echo date('Y'); ?> Recuérdame2.0
+    </div>
+</footer>
