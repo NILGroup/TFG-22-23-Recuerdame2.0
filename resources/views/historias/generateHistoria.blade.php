1856ee6155703e1695a9cbd6f538af049d76b41f
crisselene <criisalways@gmail.com>
2022-11-07 13:49:02 +0100

    múltiples opciones historias de vida

--- resources/views/historias/generateHistoria.blade.php
+++ resources/views/historias/generateHistoria.blade.php
@@ -31,46 +31,56 @@
         <div class="row">
             <label for="etapa" class="form-label col-form-label-sm col-sm-3 col-md-2 col-lg-2">Etapa de la vida</label>
             <div class="col-sm-3 col-md-3 col-lg-2">
-                <select class="form-select form-select-sm" id="idEtapa" name="idEtapa">
-                    <option></option>
+            <div class="selectBox" onclick="showCheckboxes('checkboxes')">
+                    <select class="form-select form-select-sm" id="idEtapa" name="idEtapa">
+                        <option id="seleccionadoEtapa" selected></option>
+                    </select>
+                    <div class="overSelect"></div>
+                </div>
+                <div id="checkboxes" class="checkboxes">
                     @foreach ($etapas as $etapa)
-                    <option value="{{$etapa->id}}">{{$etapa->nombre}}</option>
+                    <label> <input  type="checkbox" onclick="onSelect('{{$etapa->nombre}}', 'seleccionadoEtapa')" value={{$etapa->id}} name="seleccionEtapa[]">{{$etapa->nombre}}</label>
                     @endforeach
-
-                </select>
+                </div>
             </div>
         </div>
 
         <div class="row">
             <label for="categoria" class="form-label col-form-label-sm col-sm-3 col-md-2 col-lg-2">Categoría</label>
             <div class="col-sm-3 col-md-3 col-lg-2">
-                <select class="form-select form-select-sm" id="idCategoria" name="idCategoria" >
-                    <option ></option>
+            <div class="selectBox" onclick="showCheckboxes('checkboxesCat')">
+                    <select class="form-select form-select-sm" id="idCategoria" name="idCategoria">
+                        <option id="seleccionadoCat" selected></option>
+                    </select>
+                    <div class="overSelect"></div>
+                </div>
+                <div id="checkboxesCat" class="checkboxes">
                     @foreach ($categorias as $categoria)
-                    <option value="{{$categoria->id}}">{{$categoria->nombre}}</option>
+                    <label> <input  type="checkbox" onclick="onSelect('{{$categoria->nombre}}', 'seleccionadoCat')" value={{$categoria->id}} name="seleccionCat[]">{{$categoria->nombre}}</label>
                     @endforeach
-                </select>
+                </div>
             </div>
         </div>
 
         <div class="row">
             <label for="etiqueta" class="form-label col-form-label-sm col-sm-3 col-md-2 col-lg-2">Etiqueta</label>
             <div class="  col-sm-3 col-md-3 col-lg-2">
-                <div class="selectBox" onclick="showCheckboxes()">
+                <div class="selectBox" onclick="showCheckboxes('checkboxesEtiqueta')">
                     <select class="form-select form-select-sm" id="idEtiqueta" name="idEtiqueta">
-                        <option id="seleccionado" selected></option>
+                        <option id="seleccionadoEtiqueta" selected></option>
                     </select>
                     <div class="overSelect"></div>
                 </div>
-                <div id="checkboxes">
+                <div id="checkboxesEtiqueta" class="checkboxes">
                     @foreach ($etiquetas as $etiqueta)
-                    <label> <input  type="checkbox" onclick="onSelect('{{$etiqueta->nombre}}')" value={{$etiqueta->id}} name="seleccion[]">{{$etiqueta->nombre}}</label>
+                    <label> <input  type="checkbox" onclick="onSelect('{{$etiqueta->nombre}}','seleccionadoEtiqueta')" value={{$etiqueta->id}} name="seleccionEtiq[]">{{$etiqueta->nombre}}</label>
                     @endforeach
                 </div>
             </div>
 
         </div>
 
+        <label id="prueba"></label>
 
         <div>
             <button type="submit" name="generarLibro" value="Generar libro" class="btn btn-outline-primary ">Generar libro</button>
@@ -81,9 +91,77 @@
 </div>
 
 <script type="text/javascript">
-    $(document).ready(function() {
-        $('#idEtiqueta').multiselect();
-    });
+
+
+      //multiple seleccion con checkbox en generar Historia de vida
+      var expanded = false;
+      var expandedEtiqueta = false;
+      var expandedEtapa = false;
+      var expandedCat = false;
+
+function showCheckboxes(ide) {
+    
+    if(ide == 'checkboxesEtiqueta'){
+        var checkboxes = document.getElementById("checkboxesEtiqueta");
+        expanded = expandedEtiqueta;
+    }else if(ide == 'checkboxesCat'){
+        var checkboxes = document.getElementById("checkboxesCat");
+        expanded = expandedCat;
+    }
+    else{
+        var checkboxes = document.getElementById("checkboxes");
+        expanded = expandedEtapa;
+    }
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+
+  if(ide == 'checkboxesEtiqueta'){
+        expandedEtiqueta = expanded;
+    }else if(ide == 'checkboxesCat'){
+       expandedCat = expanded;
+    }
+    else{
+        expandedEtapa = expanded;
+    }
+
+}
+
+const array = [];
+const arrayEt = [];
+const arrayCat = [];
+const arrayEtapa = [];
+function onSelect(string, elementoSeleccionado) {
+  var select = document.getElementById(elementoSeleccionado);
+
+  if(elementoSeleccionado == 'seleccionadoEtiqueta' ){
+    array = arrayEt;
+  }else if(elementoSeleccionado == 'seleccionadoCat'){
+    array = arrayCat;
+  }else{
+    array =arrayEtapa;
+  }
+
+  const index = array.indexOf(string);
+  if (index > -1) { // only splice array when item is found
+  array.splice(index, 1); // 2nd parameter means remove one item only
+  }else  array.push(string);
+  
+  if(elementoSeleccionado == 'seleccionadoEtiqueta' ){
+    arrayEt = array;
+  }else if(elementoSeleccionado == 'seleccionadoCat'){
+    arrayCat = array;
+  }else{
+    arrayEtapa = array;
+  }
+
+  select.textContent= array;
+  //select.textContent= select.textContent+ ' ' + string;
+}
 </script>
 
 @endsection
