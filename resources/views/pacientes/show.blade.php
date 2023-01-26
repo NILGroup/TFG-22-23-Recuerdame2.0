0a18f754cc69529755549fbbbda5b4232a2aacac
Adrián Prieto <adrpriet@ucm.es>
2023-01-26 18:16:09 +0100

    Desplegable datos generales paciente

--- resources/views/pacientes/show.blade.php
+++ resources/views/pacientes/show.blade.php
@@ -7,7 +7,20 @@
         <h5 class="text-muted">Datos paciente</h5>
         <hr class="lineaTitulo">
     </div>
-    @include('pacientes.listaItems')
+    <div class="accordion mb-2"> 
+        <div class="accordion-item accordion-header" id="evaluaciones1">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#general" aria-expanded="true" aria-controls="general">
+                <div class="w-100">
+                    <h5 class="text-muted text-start">Datos Generales</h5>
+                </div>
+            </button>
+            <div id="general" class="container-fluid accordion-collapse collapse show" aria-labelledby="general">
+                @include('pacientes.listaItems')
+            </div>
+        </div>
+    </div>
+
+
     @include('pacientes.desplegables')
 
     <div class="col-12">
