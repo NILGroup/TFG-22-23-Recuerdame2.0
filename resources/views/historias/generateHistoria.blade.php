ea7be65113ea932eaafd567fca3be93e00364e0a
crisselene <criisalways@gmail.com>
2023-01-26 16:49:55 +0100

    estilo historia de vida

--- resources/views/historias/generateHistoria.blade.php
+++ resources/views/historias/generateHistoria.blade.php
@@ -10,88 +10,68 @@
     </div>
 
     <form action="/historias/generarLibro" method="GET">
-        <div class="row justify-content-start">
-            <div class="row col-sm-6 col-md-6 col-lg-5">
-                <label for="fecha" class="form-label col-form-label-sm col-sm-3 col-md-2 col-lg-5">Fecha de inicio</label>
-                <div class="col-sm-9 col-md-6 col-lg-5">
-                    <input type="date" class="form-control form-control-sm" id="fechaInicio" name="fechaInicio" value="{{$fecha}}">
-                </div>
+        <div class="d-flex justify-content-start">
+            <div class="">
+                <label for="fecha" class="form-label col-form-label-sm " style="width: 110px;">Fecha de inicio</label>
             </div>
-
-            <div class="row col-sm-6 col-md-6 col-lg-7">
-                <label for="fecha" class="form-label col-form-label-sm col-sm-3 col-md-2 col-lg-2">Fecha de fin</label>
-                <div class="col-sm-9 col-md-6 col-lg-4">
-                    <input type="date" class="form-control form-control-sm" id="fechaFin" name="fechaFin" value="{{Carbon\Carbon::now()->format('Y-m-d')}}">
-                </div>
+            <div class="fecha" style="width: fit-content;">
+                <input type="date" class="form-control form-control-sm" id="fechaInicio" name="fechaInicio" value="{{$fecha}}">
             </div>
         </div>
+
+        <div class="d-flex justify-content-start">
+            <div class="">
+                <label for="fecha" class="form-label col-form-label-sm " style="width: 110px;">Fecha de fin</label>
+            </div>
+            <div class="fecha">
+                <input type="date" class="form-control form-control-sm" id="fechaFin" name="fechaFin" value="{{Carbon\Carbon::now()->format('Y-m-d')}}">
+            </div>
+        </div>
+
 
         <input type="hidden" name="paciente_id" id="paciente_id" value="{{Session::get('paciente')['id']}}">
 
-        <div class="row">
-            <label for="etapa" class="form-label col-form-label-sm col-sm-3 col-md-2 col-lg-2">Etapa de la vida</label>
-            <div class="col-sm-9 col-md-6 col-lg-2">
-                <div class="selectBox" onclick="showCheckboxes('checkboxes')">
-                    <select class="form-select form-select-sm" id="idEtapa" name="idEtapa">
-                        <option id="seleccionadoEtapa" selected></option>
-                    </select>
-                    <div class="overSelect"></div>
-                </div>
+        <div class="row mt-3">
+            <div class="col-sm-4 col-md-3 col-lg-2 ">
+                <label for="etapa" class="form-check-label"><strong>Etapa de la vida</strong></label>
                 <div id="checkboxes" class="checkboxes">
                     @foreach ($etapas as $etapa)
-                    <label> <input type="checkbox" onclick="onSelect('{{$etapa->nombre}}', 'seleccionadoEtapa')" value={{$etapa->id}} name="seleccionEtapa[]">{{$etapa->nombre}}</label>
+                    <label> <input class="form-check-input" type="checkbox" onclick="onSelect('{{$etapa->nombre}}', 'seleccionadoEtapa')" value={{$etapa->id}} name="seleccionEtapa[]">{{$etapa->nombre}}</label>
                     @endforeach
                 </div>
             </div>
-        </div>
 
-        <div class="row">
-            <label for="categoria" class="form-label col-form-label-sm col-sm-3 col-md-2 col-lg-2">Categoría</label>
-            <div class="col-sm-3 col-md-3 col-lg-2">
-                <div class="selectBox" onclick="showCheckboxes('checkboxesCat')">
-                    <select class="form-select form-select-sm" id="idCategoria" name="idCategoria">
-                        <option id="seleccionadoCat" selected></option>
-                    </select>
-                    <div class="overSelect"></div>
-                </div>
+
+            <div class="col-sm-3 col-md-2 col-lg-2">
+                <label for="categoria" class="form-check-label"><strong>Categoría</strong></label>
                 <div id="checkboxesCat" class="checkboxes">
                     @foreach ($categorias as $categoria)
-                    <label> <input type="checkbox" onclick="onSelect('{{$categoria->nombre}}', 'seleccionadoCat')" value={{$categoria->id}} name="seleccionCat[]">{{$categoria->nombre}}</label>
+                    <label> <input type="checkbox" class="form-check-input" onclick="onSelect('{{$categoria->nombre}}', 'seleccionadoCat')" value={{$categoria->id}} name="seleccionCat[]">{{$categoria->nombre}}</label>
                     @endforeach
                 </div>
             </div>
-        </div>
 
-        <div class="row">
-            <label for="etiqueta" class="form-label col-form-label-sm col-sm-3 col-md-2 col-lg-2">Etiqueta</label>
-            <div class="  col-sm-3 col-md-3 col-lg-2">
-                <div class="selectBox" onclick="showCheckboxes('checkboxesEtiqueta')">
-                    <select class="form-select form-select-sm" id="idEtiqueta" name="idEtiqueta">
-                        <option id="seleccionadoEtiqueta" selected></option>
-                    </select>
-                    <div class="overSelect"></div>
-                </div>
+            <div class="  col-sm-3 col-md-2 col-lg-2">
+                <label for="etiqueta" class="form-check-label"><strong>Etiqueta</strong></label>
+
                 <div id="checkboxesEtiqueta" class="checkboxes">
                     @foreach ($etiquetas as $etiqueta)
-                    <label> <input type="checkbox" onclick="onSelect('{{$etiqueta->nombre}}','seleccionadoEtiqueta')" value={{$etiqueta->id}} name="seleccionEtiq[]">{{$etiqueta->nombre}}</label>
+                    <label> <input type="checkbox" class="form-check-input" onclick="onSelect('{{$etiqueta->nombre}}','seleccionadoEtiqueta')" value={{$etiqueta->id}} name="seleccionEtiq[]">{{$etiqueta->nombre}}</label>
                     @endforeach
                 </div>
             </div>
-
-            <br>
-            <br>
-            <label class=" col-form-label-sm "><strong>Tipo de recuerdos</strong></label>
-            
-            <div class="form-check">
-                <input type="hidden" name="apto" id="apto" value="1">
-                <input type="checkbox" onclick="onCheck('apto')" checked>
-                <label class=" col-form-label-sm " for="1">Aptos</label><br>
-                <input type="hidden" name="noApto" id="noApto" value="0">
-                <input type="checkbox" onclick="onCheck('noApto')">
-                <label class="form-label col-form-label-sm" for="0">No aptos</label>
-            </div>
-
         </div>
+
+        <label class="form-check-label mt-3"><strong>Tipo de recuerdos</strong></label>
+        <div class="form-check ">
+            <input type="hidden" name="apto" id="apto" value="1">
+            <input type="checkbox" class="form-check-input" onclick="onCheck('apto')" checked>
+            <label class=" col-form-label-sm " for="1">Aptos</label><br>
+            <input type="hidden" name="noApto" id="noApto" value="0">
+            <input type="checkbox" class="form-check-input" onclick="onCheck('noApto')">
+            <label class="form-label col-form-label-sm" for="0">No aptos</label>
+        </div>
+
 
 
         <div>
@@ -105,6 +85,6 @@
 @endsection
 
 @push('scripts')
-    @include('layouts.scripts')
-    <script src="/js/historiaVida.js"></script>
+@include('layouts.scripts')
+<script src="/js/historiaVida.js"></script>
 @endpush
