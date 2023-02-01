79fab6f2175935eca5d255429b84e7ca3f5de301
crisselene <criisalways@gmail.com>
2023-02-01 14:38:00 +0100

    diseño show paciente

--- resources/views/pacientes/listaItems.blade.php
+++ resources/views/pacientes/listaItems.blade.php
@@ -3,13 +3,13 @@
 <div class="row form-group justify-content-between">
     <input type="hidden" name="id" class="form-control form-control-sm" id="paciente_id" value="{{$paciente->id}}"  @if($show) disabled @endif>
     <div class="row col-sm-12 col-md-6 col-lg-5">
-        <label for="nombre" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-6">Nombre<span class="asterisco">*</span></label>
+        <label for="nombre" class="form-label col-form-label col-sm-12 col-md-12 col-lg-6 negrita">Nombre:<span class="asterisco">*</span></label>
         <div class="col-sm-12 col-md-12 col-lg-6">
             <input type="text" name="nombre" class="form-control form-control-sm" id="nombre" value="{{$paciente->nombre}}" required @if($show) disabled @endif>
         </div>
     </div>
     <div class="row col-sm-12 col-md-6 col-lg-7">
-        <label for="apellidos" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-4">Apellidos<span class="asterisco">*</span></label>
+        <label for="apellidos" class=" form-label col-form-label col-sm-12 col-md-12 col-lg-4"><strong>Apellidos:</strong><span class="asterisco">*</span></label>
         <div class="col-sm-12 col-md-12 col-lg-8">
             <input type="text" name="apellidos" class="form-control form-control-sm" id="apellidos" value="{{$paciente->apellidos}}" required @if($show) disabled @endif>
         </div>
@@ -18,7 +18,7 @@
 
 <div class="row form-group justify-content-between">
     <div class="row col-sm-12 col-md-6 col-lg-5">
-        <label for="genero" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-6">Género<span class="asterisco">*</span></label>
+        <label for="genero" class="form-label col-form-label col-sm-12 col-md-12 col-lg-6 negrita">Género:<span class="asterisco">*</span></label>
         <div class="col-sm-12 col-md-12 col-lg-6">
             <select id="genero" name="genero_id" class="form-control form-select form-select-sm" required @if($show) disabled @endif>
                 <option selected disabled></option>
@@ -30,7 +30,7 @@
     </div>
     
     <div class="row col-sm-12 col-md-6 col-lg-7">
-        <label for="pais" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-4">Nacionalidad<span class="asterisco">*</span></label>
+        <label for="pais" class="form-label col-form-label col-sm-12 col-md-12 col-lg-4 negrita">Nacionalidad:<span class="asterisco">*</span></label>
         <div class="col-sm-12 col-md-12 col-lg-8">
             <input type="text" name="nacionalidad" class="form-control form-control-sm" id="pais" value="{{$paciente->nacionalidad}}" required @if($show) disabled @endif>
         </div>
@@ -39,14 +39,14 @@
 
 <div class="row form-group justify-content-between">
     <div class="row col-sm-12 col-md-6 col-lg-5">
-        <label for="fecha" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-6">Fecha de nacimiento<span class="asterisco">*</span></label>
+        <label for="fecha" class="form-label col-form-label col-sm-12 col-md-12 col-lg-6 negrita">Fecha de nacimiento:<span class="asterisco">*</span></label>
         <div class="col-sm-12 col-md-12 col-lg-6">
             <input type="date" name="fecha_nacimiento" class="form-control form-control-sm" id="fecha" value="{{$paciente->fecha_nacimiento}}"required @if($show) disabled @endif>
         </div>
     </div>
 
     <div class="row col-sm-12 col-md-6 col-lg-7">
-        <label for="lugarNacimiento" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-4">Lugar de nacimiento<span class="asterisco">*</span></label>
+        <label for="lugarNacimiento" class="form-label col-form-label col-sm-12 col-md-12 col-lg-4 negrita">Lugar de nacimiento:<span class="asterisco">*</span></label>
         <div class="col-sm-12 col-md-12 col-lg-8">
             <input type="text" name="lugar_nacimiento" class="form-control form-control-sm" id="lugarNacimiento" value="{{$paciente->lugar_nacimiento}}" required @if($show) disabled @endif>
         </div>
@@ -55,7 +55,7 @@
 
 <div class="row form-group justify-content-between">
     <div class="row col-sm-12 col-md-6 col-lg-5">
-        <label for="estado" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-6">Estado civil<span class="asterisco">*</span></label>
+        <label for="estado" class="form-label col-form-label col-sm-12 col-md-12 col-lg-6 negrita">Estado civil:<span class="asterisco">*</span></label>
         <div class="col-sm-12 col-md-12 col-lg-6">
             <select id="estado" name="situacion_id" class="form-control form-select form-select-sm" required @if($show) disabled @endif>
                 <option selected disabled></option>
@@ -67,7 +67,7 @@
     </div>
 
     <div class="row col-sm-12 col-md-6 col-lg-7">
-        <label for="ocupacion" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-4">Ocupación<span class="asterisco">*</span></label>
+        <label for="ocupacion" class="form-label col-form-label col-sm-12 col-md-12 col-lg-4 negrita">Ocupación:<span class="asterisco">*</span></label>
         <div class="col-sm-12 col-md-12 col-lg-8">
             <input type="text" name="ocupacion" class="form-control form-control-sm" id="ocupacion" value="{{$paciente->ocupacion}}" required @if($show) disabled @endif>
         </div>
@@ -76,7 +76,7 @@
 
 <div class="row form-group justify-content-between">
     <div class="row col-sm-12 col-md-6 col-lg-5">
-        <label for="estudios" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-6">Nivel de estudios<span class="asterisco">*</span></label>
+        <label for="estudios" class="form-label col-form-label col-sm-12 col-md-12 col-lg-6 negrita">Nivel de estudios:<span class="asterisco">*</span></label>
         <div class="col-sm-12 col-md-12 col-lg-6">
             <select id="estudios" name="estudio_id" class="form-control form-select form-select-sm" required @if($show) disabled @endif>
                 <option selected disabled></option>
@@ -87,7 +87,7 @@
         </div>
     </div>
     <div class="row col-sm-12 col-md-6 col-lg-7">
-        <label for="casa" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-4">Dirección del domicilio<span class="asterisco">*</span></label>
+        <label for="casa" class="form-label col-form-label col-sm-12 col-md-12 col-lg-4 negrita">Dirección del domicilio:<span class="asterisco">*</span></label>
         <div class="col-sm-12 col-md-12 col-lg-8">
             <input type="text" name="residencia_actual" class="form-control form-control-sm" id="casa" value="{{$paciente->residencia_actual}}" required @if($show) disabled @endif>
         </div>
@@ -96,7 +96,7 @@
 
 <div class="row form-group justify-content-between">
     <div class="row col-sm-12 col-md-6 col-lg-5">
-        <label for="residencia" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-6">Tipo de residencia<span class="asterisco">*</span></label>
+        <label for="residencia" class="form-label col-form-label col-sm-12 col-md-12 col-lg-6 negrita">Tipo de residencia:<span class="asterisco">*</span></label>
         
         <div class="col-sm-12 col-md-12 col-lg-6">
             <select id="residencia" onchange="especifiqueResidencia()" style="margin-right: 5px" id="residencia" name="residencia_id" class="form-control form-select form-select-sm" required @if($show) disabled @endif>
