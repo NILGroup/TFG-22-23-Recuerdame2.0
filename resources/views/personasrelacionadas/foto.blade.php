71deecae05e4ffe38c1864f83ed66095cc67974f
crisselene <criisalways@gmail.com>
2023-02-03 11:20:53 +0100

    estilo persona  relacionada

--- resources/views/personasrelacionadas/foto.blade.php
+++ resources/views/personasrelacionadas/foto.blade.php
@@ -2,10 +2,11 @@
 <div class="row mb-4">
     <div class="img-wrap text-center w-25 mx-auto">
         @if (isset($persona->multimedia))
-        <a href="#" class="visualizarImagen"><img src="{{$persona->multimedia->fichero}}" class=" img-responsive-sm img-thumbnail"></a>
+        <a href="#" class="visualizarImagen"><img src="{{$persona->multimedia->fichero}}" class="w-25 img-responsive-sm img-thumbnail" style="width: 10em;"></a>
         @else
-        <a href="#" class="visualizarImagen"><img src="/img/avatar_hombre.png" class="img-responsive-sm img-thumbnail"></a>
+        <a href="#" class="visualizarImagen"><img src="/img/avatar_hombre.png" class="img-responsive-sm img-thumbnail" style="width: 10em;"></a>
         @endif
     </div>
 </div>
 @endif
+
--- resources/views/personasrelacionadas/listaItems.blade.php
+++ resources/views/personasrelacionadas/listaItems.blade.php
@@ -3,54 +3,54 @@
 <input type="hidden" name="paciente_id" class="form-control form-control-sm" id="paciente_id" value="{{$idPaciente}}" required @if($show) disabled @endif>
 <input type="hidden" name="id" class="form-control form-control-sm" id="id" value="{{$persona->id}}" required @if($show) disabled @endif>
 <div class="row form-group justify-content-between">
-    <div class="row col-sm-12 col-md-6 col-lg-5">
-        <label for="nombre" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-6">Nombre<span class="asterisco">*</span></label>
-        <div class="col-sm-12 col-md-12 col-lg-6">
+    <div class="row col-sm-12 col-md-6 col-lg-5 align-items-center">
+        <label for="nombre" class="form-label col-form-label col-sm-12 col-md-12 col-lg-6 negrita">Nombre:<span class="asterisco">*</span></label>
+        <div class="col-sm-12 col-md-12 col-lg-6 align-items-center">
             <input type="text" name="nombre" class="form-control form-control-sm" id="nombre" value="{{$persona->nombre}}" required @if($show) disabled @endif>
         </div>
     </div>
-    <div class="row col-sm-12 col-md-6 col-lg-7">
-        <label for="apellidos" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-4">Apellidos<span class="asterisco">*</span></label>
-        <div class="col-sm-12 col-md-12 col-lg-8">
+    <div class="row col-sm-12 col-md-6 col-lg-7 align-items-center">
+        <label for="apellidos" class="form-label col-form-label col-sm-12 col-md-12 col-lg-4 negrita">Apellidos:<span class="asterisco">*</span></label>
+        <div class="col-sm-12 col-md-12 col-lg-8 align-items-center">
             <input type="text" name="apellidos" class="form-control form-control-sm" id="apellidos" value="{{$persona->apellidos}}" required @if($show) disabled @endif>
         </div>
     </div>
 </div>
 
-<div class="row form-group justify-content-between">
-    <div class="row col-sm-12 col-md-6 col-lg-5">
-        <label for="telefono" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-6">Teléfono</label>
-        <div class="col-sm-12 col-md-12 col-lg-6">
+<div class="row form-group justify-content-between ">
+    <div class="row col-sm-12 col-md-6 col-lg-5 align-items-center">
+        <label for="telefono" class="form-label col-form-label col-sm-12 col-md-12 col-lg-6 negrita">Teléfono:</label>
+        <div class="col-sm-12 col-md-12 col-lg-6 align-items-center">
             <input type="tel" pattern="[0-9]{9}" name="telefono" class="form-control form-control-sm" id="telefono" value="{{$persona->telefono}}" @if($show) disabled @endif>
         </div>
     </div>
-    <div class="row col-sm-12 col-md-6 col-lg-7">
-        <label for="ocupacion" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-4">Ocupación<span class="asterisco">*</span></label>
-        <div class="col-sm-12 col-md-12 col-lg-8">
+    <div class="row col-sm-12 col-md-6 col-lg-7 align-items-center">
+        <label for="ocupacion" class="form-label col-form-label negrita col-sm-12 col-md-12 col-lg-4">Ocupación:<span class="asterisco">*</span></label>
+        <div class="col-sm-12 col-md-12 col-lg-8 align-items-center">
             <input type="text" name="ocupacion" class="form-control form-control-sm" id="ocupacion" value="{{$persona->ocupacion}}" required @if($show) disabled @endif>
         </div>
     </div>
 </div>
 
 <div class="row form-group justify-content-between">
-    <div class="row col-sm-12 col-md-6 col-lg-5">
-        <label for="email" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-6">Email<span class="asterisco">*</span></label>
-        <div class="col-sm-12 col-md-12 col-lg-6">
+    <div class="row col-sm-12 col-md-6 col-lg-5 align-items-center">
+        <label for="email" class="form-label col-form-label negrita col-sm-12 col-md-12 col-lg-6">Email:<span class="asterisco">*</span></label>
+        <div class="col-sm-12 col-md-12 col-lg-6 align-items-center">
             <input type="email" name="email" class="form-control form-control-sm" id="email" value="{{$persona->email}}" required @if($show) disabled @endif>
         </div>
     </div>
-    <div class="row col-sm-12 col-md-6 col-lg-7">
-        <label for="localidad" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-4">Localidad<span class="asterisco">*</span></label>
-        <div class="col-sm-12 col-md-12 col-lg-8">
+    <div class="row col-sm-12 col-md-6 col-lg-7 align-items-center">
+        <label for="localidad" class="form-label col-form-label negrita col-sm-12 col-md-12 col-lg-4">Localidad:<span class="asterisco">*</span></label>
+        <div class="col-sm-12 col-md-12 col-lg-8 align-items-center">
             <input type="text" name="localidad" class="form-control form-control-sm" id="localidad" value="{{$persona->localidad}}" required @if($show) disabled @endif>   
         </div>
     </div>
 </div>
 
 <div class="row form-group justify-content-between" >
-    <div class="row col-sm-12 col-md-6 col-lg-5">
-        <label for="tipo" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-6">Tipo de relación<span class="asterisco">*</span></label>
-        <div class="col-sm-12 col-md-12 col-lg-6">
+    <div class="row col-sm-12 col-md-6 col-lg-5 align-items-center">
+        <label for="tipo" class="form-label col-form-label negrita col-sm-12 col-md-12 col-lg-6">Tipo de relación:<span class="asterisco">*</span></label>
+        <div class="col-sm-12 col-md-12 col-lg-6 align-items-center">
             
             <select onchange="especifique()" style="margin-right: 5px" class="form-select form-select-sm form-control" id="tiporelacion_id" name="tiporelacion_id" @if($show) disabled @endif>
                 @foreach ($tipos as $tipo)
@@ -61,9 +61,9 @@
         </div>
     </div>
    
-    <div class="row col-sm-12 col-md-6 col-lg-7">
-        <label for="contacto" class="form-label col-form-label-sm col-sm-12 col-md-12 col-lg-4">Contacto<span class="asterisco">*</span></label>
-        <div class="col-sm-12 col-md-12 col-lg-8">
+    <div class="row col-sm-12 col-md-6 col-lg-7 align-items-center">
+        <label for="contacto" class="form-label col-form-label negrita col-sm-12 col-md-12 col-lg-4">Contacto:<span class="asterisco">*</span></label>
+        <div class="col-sm-12 col-md-12 col-lg-8 align-items-center">
             <input type="checkbox" name="contacto" class="form-check-input" id="contacto" value="x" @if($persona->contacto) checked @endif @if($show) disabled @endif>   
            
         </div>
@@ -71,7 +71,7 @@
 </div>
 <div class="row form-group justify-content-between" >
     <div class="mb-3 mt-3">
-        <label for="observaciones" class="form-label col-form-label-sm">Observaciones</label>
+        <label for="observaciones" class="form-label col-form-label negrita">Observaciones:</label>
         <textarea class="form-control form-control-sm" id="observaciones" name="observaciones" rows="3" @if($show) disabled @endif>{{$persona->observaciones}}</textarea>
     </div>
 </div>
