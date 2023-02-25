cd1e2c62d47a7ae1f748d07ee6fc3bd133cefe01
Eros Guerrero <94256335+erosguerrero@users.noreply.github.com>
2023-02-25 13:19:14 +0100

    Generación de vídeo [Sin funcionalidad]
    El botón de generar vídeo ya detecta que recuerdo debería usar. Se puede comprobar desde la consola del navegador.

--- resources/views/historias/generarLibro.blade.php
+++ resources/views/historias/generarLibro.blade.php
@@ -3,6 +3,10 @@
 @section('content')
 
 <div class="container-fluid" style="height: 80% !important">
+<div class="pt-4 pb-2">
+        <h5 class="text-muted">Libro Historia de Vida</h5>
+        <hr class="lineaTitulo">
+    </div>
     @if (is_null($listaRecuerdos)|| empty($listaRecuerdos) || $listaRecuerdos=='[]')
     <div class="carousel-inner container pt-4 pb-4">
         <div class="hv-box p-4">
@@ -101,6 +105,7 @@
     </div>
     <div class="text-center">
         <a href="{{ url()->previous() }}"><button type="button" class="btn btn-primary">Cancelar</button></a>
+        <button id="GenerarVideoHistoriaBoton" type="button" class="btn btn-primary">Generar vídeo</button>
     </div>
     @endif
 </div>
@@ -111,9 +116,25 @@
 
 
 @push('scripts')
-@include('layouts.scripts')
-<!--
-    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+    @include('layouts.scripts')
+    <script>
+        $(document).ready(function() {
+            var selectedRemember = 0;
+            $('#carouselPrincipal').on('slide.bs.carousel', function (e) {
+                //var active = $(e.target).find('.carousel-inner > .item.active'); var from = active.index();
+            var next = $(e.relatedTarget);
+            var to = next.index();
+            selectedRemember = to;
+            });
+            
+            $('#GenerarVideoHistoriaBoton').on('click', function(e){
+                console.log("Generando Vídeo del slide: "+selectedRemember);
+            });
+
+        });
+    
+    </script>
+    <!--<script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
