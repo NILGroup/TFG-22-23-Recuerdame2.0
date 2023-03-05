71cee4a8190b69f45bc4af0a791193088e546d51
Adrián Prieto <adrpriet@ucm.es>
2023-03-05 19:21:02 +0100

    Doble verificación al eliminar elementos

--- public/js/confirm.js
+++ public/js/confirm.js
@@ -1,7 +1,8 @@
 $('.confirm_delete').click(function (event) {
     var form = $(this).closest("form");
     var t = $(this).closest("table.datatable").dataTable();
-    var r = $(this).closest("td");
+    var e = $(this).closest("tr");
+    var dr = e.clone(true, true)
     event.preventDefault();
     swal.fire({
         title: '¿Seguro que desea eliminar?',
@@ -17,30 +18,95 @@ $('.confirm_delete').click(function (event) {
             cancelButton: 'btn btn-outline-dark'
         },
         buttonsStyling: false
-    })
-        .then((result) => {
-            if (result.isConfirmed) {
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: '¡Eliminado!',
+                text: 'Se han borrado los datos',
+                icon: 'success',
+                timer: 2000,
+                showConfirmButton: false,
+            }).then(function(){
+                t.api().row(e).remove().draw(); 
                 Swal.fire({
-                    title: '¡Eliminado!',
-                    text: 'Se han borrado los datos',
-                    icon: 'success',
-                    timer: 2000,
+                    template:"#borrado",
+                    position: 'top-end',
+                    backdrop: false,
+                    width:"25em",
                     showConfirmButton: false,
+                    buttonsStyling:false,
+                    timer: 5000,
+                    showCancelButton: false,
+                    timerProgressBar: true,
+                    showClass: {
+                        popup: 'animate__animated animate__fadeInDown'
+                    },
+                    hideClass: {
+                        popup: 'animate__animated animate__fadeOutUp'
+                    },
+                    didOpen: (toast) => {
+                        toast.addEventListener('mouseenter', Swal.stopTimer)
+                        toast.addEventListener('mouseleave', Swal.resumeTimer)
+                    },
                 })
-
-                $.ajax({
-                    url: form.attr('action'),
-                    type: 'delete',
-                    data: form.serialize(),
-                    success: function () {
-                        t.api().row(r).remove().draw();
+                .then((result) => {
+                    if (!result.isDenied) {
+                        form.submit()
+                    }
+                    else{
+                        t.api().row.add(dr).draw()
                     }
                 });
-
-
-            }
-        });
+            })
+        }
+    });
 });
+/*
+$('.confirm_delete').click(function deshacer(event) {
+    var form =  $(this).closest("form");
+    var t = $(this).closest("table.datatable").dataTable();
+    var e = $(this).closest("tr");
+    var dr = e.clone(true, true);
+    event.preventDefault();
+    removeFromTable(t, e);
+
+    Swal.fire({
+        template:"#borrado",
+        position: 'top-end',
+        backdrop: false,
+        width:"25em",
+        showConfirmButton: false,
+        buttonsStyling:false,
+        timer: 5000,
+        showCancelButton: false,
+        timerProgressBar: true,
+        showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+        },
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        },
+      })
+      .then((result) => {
+        if (!result.isDenied) {
+            form.submit()
+        }
+        else{
+            t.api().row.add(dr).draw()
+        }
+    });
+});
+*/
+
+/*
+*/
+
+
+//CALENDARIO
 $('.confirm_delete_calendario').click(function (event) {
     var form = $(this).closest("form");
     var t = $(this).closest("div .tab-pane").attr("id");
@@ -155,49 +221,3 @@ $('.confirm_finish').click(function (event) {
             }
         });
 });
-/* TO-DO Revisar para que funcione el deshacer
-$('.confirm_delete').click(function(event) {
-    var form =  $(this).closest("form");
-    var el = $(this);
-
-    event.preventDefault();
-    var t = $(this).closest("table.datatable").dataTable();
-    var e = $(this).closest("tr");
-    var ee = e.clone()
-    removeFromTable(t, e);
-
-    const Toast = Swal.fire({
-        template:"#borrado",
-        position: 'top-end',
-        backdrop: false,
-        width:"25em",
-        showConfirmButton: false,
-        buttonsStyling:false,
-        timer: 5000,
-        showCancelButton: false,
-        timerProgressBar: true,
-        showClass: {
-            popup: 'animate__animated animate__fadeInDown'
-        },
-        hideClass: {
-            popup: 'animate__animated animate__fadeOutUp'
-        },
-        didOpen: (toast) => {
-          toast.addEventListener('mouseenter', Swal.stopTimer)
-          toast.addEventListener('mouseleave', Swal.resumeTimer)
-        },
-      })
-      .then((result) => {
-        if (!result.isDenied) {
-            form.submit()
-        }
-        else{
-            location.reload();
-        }
-    });
-});
-
-function removeFromTable(t, r){
-    t.api().row(r).remove().draw(); 
-}
-*/
