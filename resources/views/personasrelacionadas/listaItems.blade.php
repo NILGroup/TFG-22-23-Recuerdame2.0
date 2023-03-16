f97a01b70a25dfe5ed5b814f3a20ef0c6239face
crisselene <criisalways@gmail.com>
2023-03-16 10:21:05 +0100

    edicion persona relacionada
    falta la vista show que se ve raro

--- resources/views/personasrelacionadas/listaItems.blade.php
+++ resources/views/personasrelacionadas/listaItems.blade.php
@@ -3,54 +3,54 @@
 <input type="hidden" name="paciente_id" class="form-control form-control-sm" id="paciente_id" value="{{$idPaciente}}" required >
 <input type="hidden" name="id" class="form-control form-control-sm" id="id" value="{{$persona->id}}" required >
 <div class="row form-group justify-content-between">
-    <div class="row col-sm-12 col-md-6 col-lg-5 align-items-center">
-        <label for="nombre" class="form-label col-form-label col-sm-12 col-md-12 col-lg-6 negrita">Nombre:<span class="asterisco">*</span></label>
-        <div class="col-sm-12 col-md-12 col-lg-6 align-items-center">
+    <div class="row col-sm-6 col-md-6 col-lg-6 align-items-center">
+        <label for="nombre" class="form-label col-form-label col-sm-4 col-md-4 col-lg-3 negrita">Nombre:<span class="asterisco">*</span></label>
+        <div class="col-sm-12 col-md-12 col-lg-7 align-items-center">
             <input type="text" name="nombre" class="form-control form-control-sm" id="nombre" value="{{$persona->nombre}}" required >
         </div>
     </div>
-    <div class="row col-sm-12 col-md-6 col-lg-7 align-items-center">
-        <label for="apellidos" class="form-label col-form-label col-sm-12 col-md-12 col-lg-4 negrita">Apellidos:<span class="asterisco">*</span></label>
-        <div class="col-sm-12 col-md-12 col-lg-8 align-items-center">
+    <div class="row col-sm-6 col-md-6 col-lg-6 align-items-center">
+        <label for="apellidos" class="form-label col-form-label col-sm-4 col-md-6 col-lg-3 negrita">Apellidos:<span class="asterisco">*</span></label>
+        <div class="col-sm-12 col-md-12 col-lg-7 align-items-center">
             <input type="text" name="apellidos" class="form-control form-control-sm" id="apellidos" value="{{$persona->apellidos}}" required >
         </div>
     </div>
 </div>
 
 <div class="row form-group justify-content-between ">
-    <div class="row col-sm-12 col-md-6 col-lg-5 align-items-center">
-        <label for="telefono" class="form-label col-form-label col-sm-12 col-md-12 col-lg-6 negrita">Teléfono:</label>
-        <div class="col-sm-12 col-md-12 col-lg-6 align-items-center">
+    <div class="row col-sm-6 col-md-6 col-lg-6 align-items-center">
+        <label for="telefono" class="form-label col-form-label col-sm-4 col-md-4 col-lg-3 negrita">Teléfono:</label>
+        <div class="col-sm-12 col-md-12 col-lg-7 align-items-center">
             <input type="tel" pattern="[0-9]{9}" name="telefono" class="form-control form-control-sm" id="telefono" value="{{$persona->telefono}}" >
         </div>
     </div>
-    <div class="row col-sm-12 col-md-6 col-lg-7 align-items-center">
-        <label for="ocupacion" class="form-label col-form-label negrita col-sm-12 col-md-12 col-lg-4">Ocupación:<span class="asterisco">*</span></label>
-        <div class="col-sm-12 col-md-12 col-lg-8 align-items-center">
+    <div class="row col-sm-6 col-md-6 col-lg-6 align-items-center">
+        <label for="ocupacion" class="form-label col-form-label negrita col-sm-4 col-md-6 col-lg-3">Ocupación:<span class="asterisco">*</span></label>
+        <div class="col-sm-12 col-md-12 col-lg-7 align-items-center">
             <input type="text" name="ocupacion" class="form-control form-control-sm" id="ocupacion" value="{{$persona->ocupacion}}" required >
         </div>
     </div>
 </div>
 
 <div class="row form-group justify-content-between">
-    <div class="row col-sm-12 col-md-6 col-lg-5 align-items-center">
-        <label for="email" class="form-label col-form-label negrita col-sm-12 col-md-12 col-lg-6">Email:<span class="asterisco">*</span></label>
-        <div class="col-sm-12 col-md-12 col-lg-6 align-items-center">
+    <div class="row col-sm-6 col-md-6 col-lg-6 align-items-center">
+        <label for="email" class="form-label col-form-label negrita col-sm-4 col-md-4 col-lg-3">Email:<span class="asterisco">*</span></label>
+        <div class="col-sm-12 col-md-12 col-lg-7 align-items-center">
             <input type="email" name="email" class="form-control form-control-sm" id="email" value="{{$persona->email}}" required >
         </div>
     </div>
-    <div class="row col-sm-12 col-md-6 col-lg-7 align-items-center">
-        <label for="localidad" class="form-label col-form-label negrita col-sm-12 col-md-12 col-lg-4">Localidad:<span class="asterisco">*</span></label>
-        <div class="col-sm-12 col-md-12 col-lg-8 align-items-center">
+    <div class="row col-sm-12 col-md-6 col-lg-6 align-items-center">
+        <label for="localidad" class="form-label col-form-label negrita col-sm-4 col-md-6 col-lg-3">Localidad:<span class="asterisco">*</span></label>
+        <div class="col-sm-12 col-md-12 col-lg-7 align-items-center">
             <input type="text" name="localidad" class="form-control form-control-sm" id="localidad" value="{{$persona->localidad}}" required >   
         </div>
     </div>
 </div>
 
 <div class="row form-group justify-content-between" >
-    <div class="row col-sm-12 col-md-6 col-lg-5 align-items-center">
-        <label for="tipo" class="form-label col-form-label negrita col-sm-12 col-md-12 col-lg-6">Tipo de relación:<span class="asterisco">*</span></label>
-        <div class="col-sm-12 col-md-12 col-lg-6 align-items-center">
+    <div class="row col-sm-6 col-md-6 col-lg-6 align-items-center">
+        <label for="tipo" class="form-label col-form-label negrita col-sm-4 col-md-4 col-lg-3">Tipo de relación:<span class="asterisco">*</span></label>
+        <div class="col-sm-12 col-md-12 col-lg-7 align-items-center">
             
             <select onchange="especifique()" style="margin-right: 5px" class="form-select form-select-sm form-control" id="tiporelacion_id" name="tiporelacion_id" >
                 @foreach ($tipos as $tipo)
@@ -61,9 +61,9 @@
         </div>
     </div>
    
-    <div class="row col-sm-12 col-md-6 col-lg-7 align-items-center">
-        <label for="contacto" class="form-label col-form-label negrita col-sm-12 col-md-12 col-lg-4" >Contacto: <i class="bi bi-question-circle" data-toggle="tooltip" data-placement="top" title="Indicar si se trata del contacto preferente para este usuario."></i><span class="asterisco">*</span></label>
-        <div class="col-sm-12 col-md-12 col-lg-8 align-items-center">
+    <div class="row col-sm-6 col-md-6 col-lg-6 align-items-center">
+        <label for="contacto" class="form-label col-form-label negrita col-sm-4 col-md-6 col-lg-3" >Contacto: <i class="bi bi-question-circle" data-toggle="tooltip" data-placement="top" title="Indicar si se trata del contacto preferente para este usuario."></i><span class="asterisco">*</span></label>
+        <div class="col-sm-12 col-md-12 col-lg-7 align-items-center">
             <input type="checkbox" name="contacto" class="form-check-input" id="contacto" value="x" @if($persona->contacto) checked @endif >   
            
         </div>
--- resources/views/personasrelacionadas/show.blade.php
+++ resources/views/personasrelacionadas/show.blade.php
@@ -7,31 +7,34 @@
         <h5 class="text-muted">Datos persona relacionada</h5>
         <hr class="lineaTitulo">
     </div>
-    @include('personasrelacionadas.foto')
-    @include('personasrelacionadas.listaItems')
-    @if (Auth::user()->rol_id == 1)
-    <div class="col-12">
-        <!-- <a href="{{ url()->previous() }}"><button type="button" class="btn btn-primary">Atrás</button></a> -->
-        <a href="/pacientes/{{$idPaciente}}/personas/{{$persona->id}}/editar"><button type="button" class="btn btn-secondary">Editar</button></a>
-    </div>
-    @endif
-</div>
-  
+    <div class="row text-align-center">
+        <div class="col-lg-3 align-items-center">
+            @include('personasrelacionadas.foto')
+        </div>
+            @include('personasrelacionadas.listaItems')
+            @if (Auth::user()->rol_id == 1)
+            <div class="col-12">
+                <!-- <a href="{{ url()->previous() }}"><button type="button" class="btn btn-primary">Atrás</button></a> -->
+                <a href="/pacientes/{{$idPaciente}}/personas/{{$persona->id}}/editar"><button type="button" class="btn btn-secondary">Editar</button></a>
+            </div>
+            @endif
+        </div>
 
-@endsection
 
-@push('scripts')
-    @include('layouts.scripts')
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="/js/showView.js"></script>
-    
-    @if (Session::has('created'))
-        @php 
-            Illuminate\Support\Facades\Session::forget('created');
+        @endsection
+
+        @push('scripts')
+        @include('layouts.scripts')
+        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script src="/js/showView.js"></script>
+
+        @if (Session::has('created'))
+        @php
+        Illuminate\Support\Facades\Session::forget('created');
         @endphp
         <script>
             var action = "Modificado"
         </script>
         <script src="/js/successAlert.js"></script>
-    @endif
-@endpush
+        @endif
+        @endpush
