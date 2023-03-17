735d0931541558553dc88aa32f33f363c5e16a15
crisselene <criisalways@gmail.com>
2023-03-17 10:59:17 +0100

    foto genero en persona relacionada

--- resources/views/personasrelacionadas/foto.blade.php
+++ resources/views/personasrelacionadas/foto.blade.php
@@ -3,7 +3,7 @@
     <div class="img-wrap text-center mx-auto">
         @if (isset($persona->multimedia))
         <a href="#" class="visualizarImagen"><img src="{{$persona->multimedia->fichero}}" class="imagenPaciente img-responsive-sm img-thumbnail" style="width: 10em;"></a>
-        @elseif ($paciente->genero_id == '2')
+        @elseif ($persona->genero_id == '2')
         <a href="#" class="visualizarImagen"><img src="/img/avatar_mujer.png" class="imagenPaciente img-responsive-sm img-thumbnail" ></a>
         @else 
         <a href="#" class="visualizarImagen"><img src="/img/avatar_hombre.png" class="imagenPaciente img-responsive-sm img-thumbnail" ></a>
