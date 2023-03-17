fe522215581c819e6f910e52aa7511f6efa9a4df
crisselene <criisalways@gmail.com>
2023-03-17 15:52:03 +0100

    register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,69 +13,81 @@
 
                     <h5 class="text-center text-muted">Registro terapeuta</h5>
 
-                    <div class="d-flex flex-row align-items-center mb-4 mt-2">
-                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                        <input id="nombre" type="text" class="form-control @error('nombre') is-invalid @enderror" placeholder="Nombre" name="nombre" value="{{ old('nombre') }}" required autocomplete="nombre" autofocus>
-
+                    <div class="d-block-inline align-items-center mt-4">
+                        <div class="d-flex flex-row align-items-center">
+                            <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                            <input id="nombre" type="text" class="form-control @error('nombre') is-invalid @enderror" placeholder="Nombre" name="nombre" value="{{ old('nombre') }}" required autocomplete="nombre" autofocus>
+                        </div>
                         @error('nombre')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                        <span role="alert" class="msgErrorRegister">
+                            <strong>{{ $message }}</strong>
+                        </span>
                         @enderror
                     </div>
 
-                    <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                        <input id="apellidos" type="text" class="form-control @error('apellidos') is-invalid @enderror" placeholder="Apellidos" name="apellidos" value="{{ old('apellidos') }}" required autocomplete="apellidos" autofocus>
-
+                    <div class="d-block-inline align-items-center mt-4">
+                        <div class="d-flex flex-row align-items-center">
+                            <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                            <input id="apellidos" type="text" class="form-control @error('apellidos') is-invalid @enderror" placeholder="Apellidos" name="apellidos" value="{{ old('apellidos') }}" required autocomplete="apellidos" autofocus>
+                        </div>
                         @error('apellidos')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                        <span role="alert" class="msgErrorRegister">
+                            <strong>{{ $message }}</strong>
+                        </span>
                         @enderror
                     </div>
 
-                    <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                        <input id="usuario" type="text" class="form-control @error('usuario') is-invalid @enderror" placeholder="Nombre de Usuario" name="usuario" value="{{ old('usuario') }}" required autocomplete="usuario" autofocus>
-
+                    <div class="d-block-inline align-items-center mt-4">
+                        <div class="d-flex align-items-center ">
+                            <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                            <input id="usuario" type="text" class="form-control @error('usuario') is-invalid @enderror" placeholder="Nombre de Usuario" name="usuario" value="{{ old('usuario') }}" required autocomplete="usuario" autofocus>
+                        </div>
                         @error('usuario')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                        <span role="alert" class="msgErrorRegister">
+                            <strong>{{ $message }}</strong>
+                        </span>
                         @enderror
                     </div>
 
-                    <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Correo Electrónico" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                    <div class="d-block-inline align-items-center mt-4">
+                        <div class="d-flex align-items-center ">
+                            <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Correo Electrónico" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                            @error('email')
+                        </div>
+                        <span role="alert" class="msgErrorRegister"><strong>{{ $message }}</strong>
+                        </span>
                         @enderror
                     </div>
 
-                    <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                    <div class="d-block-inline align-items-center mt-4 pb-4">
+                        <div class="d-flex flex-row align-items-center ">
+                            <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                             <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Contraseña" name="password" required autocomplete="new-password">
-
                             @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
+                    </div>
+                        <span role="alert" class=" msgErrorRegister">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                        
                     </div>
 
-                    <div class="d-flex flex-row align-items-center mb-4">
+
+                    <div class="d-block-inline align-items-center ">
+                    <div class="d-flex flex-row align-items-center ">
                         <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Confirmar Contraseña" required autocomplete="new-password">
+                        <input id="password-confirm" type="password" class=" form-control" name="password_confirmation" placeholder="Confirmar Contraseña" required autocomplete="new-password">
+                        
+                    </div>
+                        
                     </div>
 
-                    <input id="rol" type="hidden" name="rol" value=1 required autocomplete="apellidos" autofocus>
+                    <input id="rol" class=mt-4" type="hidden" name="rol" value=1 required autocomplete="apellidos" autofocus>
 
-                    <div class="d-grid gap-2">
+                    <div class="d-grid gap-2 mt-4">
                         <button type="submit" id="registrarNuevo" class="btn btn-lg btn-primary">{{ __('Registrar') }}</button>
                     </div>
                 </div>
