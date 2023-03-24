928abe1c0c30e0ee98a71e6b1337c9863d0c6382
Adrián Prieto <adrpriet@ucm.es>
2023-03-24 09:59:53 +0100

    Touchspin mismo tamaño

--- public/js/informeSeguimiento.js
+++ public/js/informeSeguimiento.js
@@ -2,7 +2,10 @@
 $(document).ready(function () {
     $("input[type='number']").each(function(){
         if($(this).attr("max")){
-            var v =  "/" + $(this).attr('max');
+            var v =  "/ " + $(this).attr('max');
+            if(v.length < 4){
+                v = v+"  "
+            }
             $(this).TouchSpin({
                 buttondown_class:"hidden",
                 buttonup_class:"hidden",
