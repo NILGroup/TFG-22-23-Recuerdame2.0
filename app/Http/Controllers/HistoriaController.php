945693404ef1315d572313ba2db12d0852be3889
crisselene <criisalways@gmail.com>
2023-04-25 12:56:44 +0200

    filtro HV puntuacion (falta cambiar el diseño y el filtro por fecha)

--- app/Http/Controllers/HistoriaController.php
+++ app/Http/Controllers/HistoriaController.php
@@ -7,6 +7,7 @@ use App\Models\Paciente;
 use App\Models\Etapa;
 use App\Models\Categoria;
 use App\Models\Etiqueta;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 
 use function PHPUnit\Framework\returnSelf;
 
@@ -45,26 +46,52 @@ class HistoriaController extends Controller
         $fechaInicio = $request->fechaInicio;
         $fechaFin = $request->fechaFin;
         $idEtapa = $request->seleccionEtapa;
-        $idEtiqueta = $request->seleccionEtiq;
+        $puntuacion = $request->seleccionEtiq;
         $idCategoria = $request->seleccionCat;
         $apto = $request->apto;
         $noApto = $request->noApto;
         $paciente = Paciente::find($idPaciente);
-        
+        $puntuacionFinal = collect([]);
+       
         if (is_null($idEtapa))
             $idEtapa = Etapa::select('id');
-        if (is_null($idEtiqueta))
-            $idEtiqueta = Etiqueta::select('id');
         if (is_null($idCategoria))
             $idCategoria = Categoria::select('id');
+        if (is_null($puntuacion)){
+            $puntuacionFinal = collect([0,1,2,3,4,5,6,7,8,9,10]);
+        }else {
+            
+            if(in_array("1", $puntuacion))
+            {
+                $puntuacionFinal->push(6,7,8,9,10);
+            }
+            if(in_array("2", $puntuacion))
+            {
+                $puntuacionFinal->push(5);
+            }
+            if(in_array("3", $puntuacion))
+            {
+                $puntuacionFinal->push(0,1,2,3,4);
+            }
+        }
+                
 
         $listaRecuerdos =  $paciente->recuerdos()
-            ->whereIn('etapa_id', $idEtapa)->orWhereNull('etapa_id')
-            ->whereIn('etiqueta_id', $idEtiqueta)->orWhereNull('etiqueta_id')
-            ->whereIn('categoria_id', $idCategoria)->orWhereNull('categoria_id')
-            ->whereBetween('fecha', [$fechaInicio, $fechaFin])
+           ->where(function($query) use ($idEtapa){
+            $query->whereIn('etapa_id', $idEtapa)->orWhereNull('etapa_id');
+
+           })->where(function($query) use ($idCategoria){
+            $query->whereIn('categoria_id',$idCategoria)->orWhereNull('categoria_id');
+                      
+           })->where(function($query) use ($puntuacionFinal){
+            $query ->whereIn('puntuacion', $puntuacionFinal)->orWhereNull('puntuacion');
+           })
             ->get();
-        //return $listaRecuerdos;
+            /*->whereIn('etiqueta_id', $idEtiqueta)->orWhereNull('etiqueta_id') */
+
+            /*->whereBetween('fecha', [$fechaInicio, $fechaFin])*/
+
+        return $listaRecuerdos;
         if(!($apto == 0 && $noApto == 0) && !($apto == 1 && $noApto == 1))
             $listaRecuerdos = $listaRecuerdos
                 ->whereIn('apto', $apto);
