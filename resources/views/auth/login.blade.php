9c5ab399ef90a5244c25dad9255afbbdab84f703
Adrián Prieto <72133257+Lurngi@users.noreply.github.com>
2023-05-03 20:12:58 +0200

    Más cambios login y registro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -46,9 +46,7 @@
                             <p></p>
                             <p></p>
                             @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('He olvidado mi contraseña') }}
-                                </a>
+                                <!-- <a class="btn btn-link" href="{{ route('password.request') }}">{{ __('He olvidado mi contraseña') }}</a> -->
                             @endif
                         </div>
                     </form>
@@ -65,13 +63,9 @@
 
 
 @push('scripts')
-
-    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/dropzone.min.js"></script>
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    @include('layouts.scripts')
     <script src="/js/general.js"></script>
+    <script src="/js/libs/sweetAlert2.js"></script>
     <script>
         $("#rellenaBBDD").on("click", function(event){
             event.stopPropagation()
