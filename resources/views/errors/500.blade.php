7770f26e6012534d308f049d003c3e6490869a3f
crisselene <criisalways@gmail.com>
2023-05-07 00:19:48 +0200

    página de error 500 y styles errores

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="/css/errorStyle.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="fondo">
+  
+      <div class="text-center fondo">
+        <div class="col-lg-6 offset-lg-3 col-sm-6 offset-sm-3 col-12 p-3 error-main">
+          <div class="row">
+            <div class="col-lg-8 col-12 col-sm-10 offset-lg-2 offset-sm-1">
+            <div class="bg-image text-center mb-1"><a class="navbar-brand " href="{{ url('/') }}"><img class="logotipoMarca w-25" src="/img/Marca_recuerdame-nobg.png" /></a></div>
+
+              <h1 class="m-0">500</h1>
+              <h6>Internal Server Error</h6>
+              <p>Error interno del servidor.
+              Inténtelo más tarde o contacte con soporte.
+              </p>
+             
+            </div>
+          </div>
+        </div>
+      </div>
+  
+</body>
+</html>
