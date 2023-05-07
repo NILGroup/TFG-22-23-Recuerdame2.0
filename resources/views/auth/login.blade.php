3f118d09bd6c11f129dfd712a66c7c03965966e7
Adrián Prieto <72133257+Lurngi@users.noreply.github.com>
2023-05-07 15:59:13 +0200

    Prueba 2 docker

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    asdvasdvasdvasdvasdv
+    a
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card-body">
