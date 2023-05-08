779eedcf21c932025728aed1066080d96af2b1df
Eros Guerrero <94256335+erosguerrero@users.noreply.github.com>
2023-05-08 22:23:56 +0200

    Update video-mail.blade.php

--- resources/views/mail/video-mail.blade.php
+++ resources/views/mail/video-mail.blade.php
@@ -9,5 +9,5 @@ Ver vídeo
 
 Gracias por usar nuestros servicios,<br>
 El equipo de {{ config('app.name') }}
-<img src="$logo" class="img-header">
+<img src={{$logo}} class="img-header">
 </x-mail::message>
